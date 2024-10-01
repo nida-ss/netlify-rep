9baee92308bae1f00b021fcda3c8ecfd3597aafc
nida-ss <nida.ideal@gmail.com>
2024-10-01 18:42:08 +0500

    changes in app.js

--- my_web/src/App.js
+++ my_web/src/App.js
@@ -1,15 +1,30 @@
-// import logo from './logo.svg';
-import './App.css';
-
+// src/App.js
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Button, Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <div>
-   
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">My React App</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-5">
+        <h1 className="text-center">Welcome to My React App</h1>
+        <div className="text-center">
+          <Button variant="primary">Click Me</Button>
+        </div>
+      </Container>
+    </>
   );
 }
 
 export default App;
- 
- 
